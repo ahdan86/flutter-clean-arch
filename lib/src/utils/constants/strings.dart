b3608bb03afc3f73890c83235d8a11a0988bf9e7
Ahdan Amanullah <ahdanamanullah@gmail.com>
2023-10-24 10:58:01 +0700

    Init Project

---
+++ lib/src/utils/constants/strings.dart
@@ -0,0 +1,11 @@
+// App
+const String appTitle = 'Flutter Clean Architecture';
+
+// Networking and APIs
+const String baseUrl = 'https://newsapi.org/v2';
+const String defaultApiKey = '6878623c932448be9a1fc9c8d69936bf';
+const String defaultSources = 'bbc-news, abc-news, al-jazeera-english';
+
+// Storage and Databases
+const String articlesTableName = 'articles_table';
+const String databaseName = 'app_database.db';
